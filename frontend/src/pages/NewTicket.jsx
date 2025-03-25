20ae6a69f442b4f84707c52628774323eb79f05e
Rqmba <ramblack45@gmail.com>
2025-03-25 14:32:12 +0100

    New ticket form, add Tickets to Redux

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,10 +1,96 @@
-import React from "react";
-
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { createTicket, reset } from "../features/tickets/ticketSlice";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
 function NewTicket() {
+  const { user } = useSelector((state) => state.auth);
+  const { isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.ticket
+  );
+
+  const [name] = useState(user.name);
+  const [email] = useState(user.email);
+  const [product, setProduct] = useState("iPhone");
+  const [description, setDescription] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess) {
+      dispatch(reset());
+      navigate("/tickets");
+    }
+
+    dispatch(reset());
+  }, [dispatch, isError, isSuccess, navigate, message]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createTicket({ product, description }));
+  };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
-    <div>
-      <h1>Nouvelle demande</h1>
-    </div>
+    <>
+      <BackButton url="/" />
+      <section className="heading">
+        <h1>Crée une demande</h1>
+        <p>Veuillez remplir le formulaire ci-dessous</p>
+      </section>
+
+      <section className="form">
+        <div className="form-group">
+          <label htmlFor="name">Nom du client </label>
+          <input type="text" className="form-control" value={name} disabled />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Email du client</label>
+          <input type="text" className="form-control" value={email} disabled />
+        </div>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="product">Produit</label>
+            <select
+              name="product"
+              id="product"
+              value={product}
+              onChange={(e) => setProduct(e.target.value)}
+            >
+              <option value="iPhone">iPhone</option>
+              <option value="iPad">iPad</option>
+              <option value="Macbook Pro">Macbook Pro</option>
+              <option value="Macbook Air">Macbook Air</option>
+              <option value="iMac">iMac</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description du problème</label>
+            <textarea
+              name="description"
+              id="description"
+              className="form-control"
+              placeholder="Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Envoyez</button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 }
 
